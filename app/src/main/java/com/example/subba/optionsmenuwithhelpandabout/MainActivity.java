53735d7c7907cbf8377dd0b4b92aa4935d76b8ff
kavi009 <kaviramya009@gmail.com>
2016-05-30 22:00:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/subba/optionsmenuwithhelpandabout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.subba.optionsmenuwithhelpandabout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}
